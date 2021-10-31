0768ec1612e64b236caa5942d8cfdf76ed96e9c8
Baullin1767 <starkov.alexd@gmail.com>
2021-10-31 18:54:38 +0500

    Version 0.2

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -14,23 +14,42 @@ public class MouseLook : MonoBehaviour
 
     PhotonView photonView;
 
+    bool mouseLock = false;
+
     void Start()
     {
         photonView = GetComponent<PhotonView>();
-        Cursor.lockState = CursorLockMode.Locked;
     }
 
     
     void Update()
     {
         if (!photonView.IsMine) { return; }
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            if (mouseLock == true)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                mouseLock = false;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                mouseLock = true;
+            }
+        }
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+        if (mouseLock)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX); 
+        }
     }
 }
