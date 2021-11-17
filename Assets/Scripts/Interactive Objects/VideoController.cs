fcfc2c4310940a3cc13429103c9dc5ebf7e32395
Baullin1767 <starkov.alexd@gmail.com>
2021-11-18 00:45:50 +0500

    Final Version

--- Assets/Scripts/Interactive Objects/VideoController.cs
+++ Assets/Scripts/Interactive Objects/VideoController.cs
@@ -2,16 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VideoController : MonoBehaviour
+public class VideoController : _InteractiveObject
 {
     UnityEngine.Video.VideoPlayer videoPlayer;
-    private void Start()
+    protected override void Start()
     {
         videoPlayer = GetComponent<UnityEngine.Video.VideoPlayer>();
     }
 
-    public void Play() 
+    void Play() 
     {
         videoPlayer.Play();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            videoPlayer.Play(); 
+        }
+    }
+
+    void OnTriggerExit(Collider other) 
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            videoPlayer.Pause(); 
+        }
     }
 }
